90f1ba1ca61cf2d570ddb5f500064ed6f2bfbf2b
Ian Mignaco <ian.mignaco@alphabird.com>
2014-01-24 17:05:49 +1100

    Test nav

--- flickr/index.php
+++ flickr/index.php
@@ -1,11 +1,22 @@
 <?php
     require_once('model.php');
     require_once('view.php');
+	
+	function showResults($query, $searchModel, $page=1) {
+		$queryResult = $searchModel->getDataFromFlickr($query,$page);
+		if($queryResult){
+			// Show the images
+			$searchView->renderImgListView($query, $searchModel);
+		} else {
+			// Show error message
+		}
+	}
     
     $searchModel = new FlickrModel();
 	$searchView = new FlickrMainView();
 	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-		$queryResult = $searchModel->getDataFromFlickr($_POST['search']);
+		// showResults($_POST['search'], $searchModel);
+		$queryResult = $searchModel->getDataFromFlickr($_POST['search'],$page);
 		if($queryResult){
 			// Show the images
 			$searchView->renderImgListView($_POST['search'], $searchModel);
@@ -18,15 +29,6 @@
 		// } else {
 			// $searchView->renderDefaultHomeView();
 		// }
+		$searchView->renderDefaultHomeView();
 	}
-	
-	// function showResults($query, $searchModel, $page=1) {
-		// $queryResult = $searchModel->getDataFromFlickr($query,$page);
-		// if($queryResult){
-			// // Show the images
-			// $searchView->renderImgListView($query, $searchModel);
-		// } else {
-			// // Show error message
-		// }
-	// }
 ?>
